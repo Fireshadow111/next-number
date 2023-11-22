460ab525904dba4862c9c2545f13466590e697dd
Fireshadow111 <muhammadrajah16@gmail.com>
2023-11-22 15:33:53 +0200

    adding

--- script/index.js
+++ script/index.js
@@ -3,19 +3,15 @@
         let calButton = document.getElementById('calButton');
 
       //Creating array
-        let numberArray = [1, 7, 11, 22, 44, 77];
+        let numArray = [1, 7, 11, 22, 44, 77];
+        let i = 0
 
-        //function to check if array is empty
+        //function, if i is less than the array length then it will cycle through the array
         function nextNum() {
-            if (numberArray.length === 0) {
-                alert('Array is empty.');
-                return;
-            }
-
-            nextNum.currentIndex = (nextNum.currentIndex + 1) % numberArray.length || 0;44
-
-            //display the next number in the array
-            nextOutput.value = numberArray[nextNum.currentIndex];
+          if(i<numArray.length){
+          let next = numArray[i];
+          i++
+          nextOutput.value = next
         }
-        nextNum.currentIndex = -1;
-        calButton.addEventListener('click', nextNum);
+        }
+        calButton.addEventListener('click', nextNum)
